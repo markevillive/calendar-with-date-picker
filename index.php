9d32a82cc0ff694bda300d025e851c239c2c73e4
markevillive <mark_evillive@hotmail.com>
2020-09-14 02:27:53 +0700

    start

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+   <meta charset="UTF-8">
+    <title>Calendar</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+<header>
+    <div class="container">
+        <h1>Функциональный календарь на Php</h1>
+    </div>
+
+</header>
+<div class="container">
+    <section>
+        <div class="container">
+            <?php
+            
+            ?>
+        </div>
+    </section>
+</div>
+<footer>
+    <div class="navbar-fixed-bottom row-fluid">
+        <div class="navbar-inner">
+            <div class="container">
+                <h6>&copy;Marc Shreder 2020</h6>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
