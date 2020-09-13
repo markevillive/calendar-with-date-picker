0c4199a459d1e7c426cefd8a14c678008d1672c7
markevillive <mark_evillive@hotmail.com>
2020-09-14 06:58:35 +0700

    addCss

--- index.php
+++ index.php
@@ -4,6 +4,40 @@
    <meta charset="UTF-8">
     <title>Calendar</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <style type="text/css">
+
+        .block-on-center {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-top: -100px;
+            margin-left: -100px; }
+
+        .cal{
+            border: 1px solid #ccc;
+            color: #333;
+            background: #F6F6F6;
+            font: Arial;
+            font-size: 14px;
+            text-align: center;}
+
+        .caltoday{
+            font: Arial;
+            font-size: 14px;
+            text-align: center;
+            font-weight: bold;}
+
+        .navi{
+            font: Arial;
+            font-size: 16px;
+            font-weight: bold;}
+
+        .datehead{
+            font: Arial;
+            font-size: 16px;
+            font-weight: bold;}
+
+    </style>
 </head>
 <body>
 <header>
